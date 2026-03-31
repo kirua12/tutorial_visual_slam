961cc01879f6d7068ac612083300e7f903565e19
kirua12 <kch8448ch@naver.com>
2026-04-01 08:46:55 +0900

    cmake and eigen

---
+++ Eigen_test/build/CMakeFiles/Eigen_Matrix.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Eigen_Matrix.dir/egienMatrix.cpp.o"
+  "CMakeFiles/Eigen_Matrix.dir/egienMatrix.cpp.o.d"
+  "Eigen_Matrix"
+  "Eigen_Matrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Eigen_Matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
